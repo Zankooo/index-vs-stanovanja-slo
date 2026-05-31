c3d53730ff59375d58e27c85f908617ceb33ba7a
Zankooo <stankovic.zan@gmail.com>
2026-05-31 22:02:36 +0200

    popravki mini

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Feedback from './views/Feedback.vue'
+import { Analytics } from "@vercel/analytics/vue"
+
+
+<Analytics/>
 
 const routes = [
   { path: '/', component: Home },
